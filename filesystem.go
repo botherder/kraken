c2fc45fd23a5ca9a1b47d7fea4681d9b5fd5e4a2
Nex <nex@nex.sx>
2018-09-23 15:45:10 +0200

    Adding skeleton for filesystem scanning

--- filesystem.go
+++ filesystem.go
@@ -1,3 +1,19 @@
+// Kraken
+// Copyright (C) 2016-2018  Claudio Guarnieri
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -19,9 +35,8 @@ func fileDetected(filePath, signature string) *Detection {
 }
 
 func filesystemScan() (detections []*Detection) {
-	// TODO: Add proper discovery of root.
-	root := "/"
-	filepath.Walk(root, func(filePath string, fileHandle os.FileInfo, err error) error {
+	root := getFileSystemRoot()
+	filepath.Walk(root, func(filePath string, fileInfo os.FileInfo, err error) error {
 		log.Debug("Scanning file ", filePath)
 		matches, _ := scanner.ScanFile(filePath)
 		for _, match := range matches {
