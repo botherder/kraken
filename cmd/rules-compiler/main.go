bbddb693302dccb3b988670310547ad37c082bd8
Nex <nex@nex.sx>
2021-01-03 21:13:39 +0100

    Now using scanner package to compile rules

--- cmd/rules-compiler/main.go
+++ cmd/rules-compiler/main.go
@@ -17,63 +17,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path"
-	"path/filepath"
 
-	"github.com/hillu/go-yara/v4"
+	"github.com/botherder/kraken/scanner"
 )
 
 func main() {
-	// The argument is the path to the rule or a folder containing rules.
-	rulesPath := os.Args[1]
-
-	// Check if the path exists.
-	if _, err := os.Stat(rulesPath); os.IsNotExist(err) {
-		panic(err)
-	}
-
-	// Instantiate the Yara compiler.
-	compiler, err := yara.NewCompiler()
+	rules, err := scanner.Compile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
-	rulesStat, _ := os.Stat(rulesPath)
-	switch mode := rulesStat.Mode(); {
-	// Check if the path is a folder...
-	case mode.IsDir():
-		fmt.Println("[rules-compiler] The specified path is a folder, looping through files...")
-		err = filepath.Walk(rulesPath, func(filePath string, fileObj os.FileInfo, err error) error {
-			// Get the file name.
-			fileName := fileObj.Name()
-
-			// Check if the file has extension .yar or .yara.
-			if (path.Ext(fileName) == ".yar") || (path.Ext(fileName) == ".yara") {
-				fmt.Println("[rules-compiler] Adding rule", filePath)
-
-				// Open the rule file and add it to the Yara compiler.
-				rulesObj, _ := os.Open(filePath)
-				defer rulesObj.Close()
-				compiler.AddFile(rulesObj, "")
-			}
-			return nil
-		})
-	// Check if it is a file instead...
-	case mode.IsRegular():
-		fmt.Println("[rules-compiler] Compiling Yara rule", rulesPath)
-		// Open the rule file and add it to the Yara compiler.
-		rulesObj, _ := os.Open(rulesPath)
-		defer rulesObj.Close()
-		compiler.AddFile(rulesObj, "")
-	}
-
-	// Collect and compile Yara rules.
-	rules, _ := compiler.GetRules()
-
-	// Save the compiled rules to a file.
 	rules.Save("rules")
-
-	fmt.Println("[rules-compiler] Done!")
 }
