fc01bdcdd2977b57886a190a6e015041299a2ff6
Nex <nex@nex.sx>
2021-01-04 11:43:21 +0100

    Adding API function for heartbeats

---
+++ api/heartbeat.go
@@ -0,0 +1,44 @@
+// This file is part of Kraken (https://github.com/botherder/kraken)
+// Copyright (C) 2016-2021  Claudio Guarnieri
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package api
+
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// Sends an heartbeat to the API server.
+func (a *API) Heartbeat() error {
+	client := resty.New()
+	response, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(fmt.Sprintf(`{"identifier":"%s"}`, a.MachineID)).
+		Post(a.URLToHeartbeat)
+
+	// Check if request failed.
+	if err != nil {
+		return fmt.Errorf("Unable to send heartbeat to API server: %s", err)
+	}
+
+	// Check if the response wasn't right.
+	if response.StatusCode() != 200 {
+		return fmt.Errorf("Unable to send heartbeat to API server: we received response code %d", response.StatusCode())
+	}
+
+	return nil
+}
